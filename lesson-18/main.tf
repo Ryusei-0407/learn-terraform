672b022c67585aa624a7fca487200cbdad4b9134
Ryusei-0407 <ryu000407@gmail.com>
2021-08-06 18:27:45 +0900

    edit terraform format

--- lesson-18/main.tf
+++ lesson-18/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy" "s3_full_access" {
-  name = "s3_full_access"
-  path = "/"
+  name        = "s3_full_access"
+  path        = "/"
   description = "s3_full_access"
 
   policy = <<EOF
