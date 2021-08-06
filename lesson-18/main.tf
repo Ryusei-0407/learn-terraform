2858736f417e91dc2407360641907607a0fd9aaa
Ryusei-0407 <ryu000407@gmail.com>
2021-08-06 17:39:17 +0900

    create aim policy role

---
+++ lesson-18/main.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_policy" "s3_full_access" {
+  name = "s3_full_access"
+  path = "/"
+  description = "s3_full_access"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "s3_full_access_policy" {
+  name = "s3_full_access_policy"
+  role = aws_iam_role.s3_full_access.id
+
+  policy = aws_iam_policy.s3_full_access.policy
+}
+
+resource "aws_iam_role" "s3_full_access" {
+  name = "s3_full_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumRole",
+      "Principal": {
+        "Service": "ec2.amazon.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
